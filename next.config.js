209973324e34813aac084f0f25f60080760b6059
iamcodemonga <codemonga@gmail.com>
2023-03-26 12:07:38 +0100

    updated imports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Include components directory in webpack configuration
+    config.module.rules.push({
+      test: /\.jsx?$/,
+      include: /components/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['next/babel'],
+        },
+      },
+    });
+    return config;
+  },
   images: {
     domains: ["images.pexels.com"]
   }
