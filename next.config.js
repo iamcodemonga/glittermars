c139f1a7f8b760174f08070c1e6935653b7d4550
iamcodemonga <codemonga@gmail.com>
2023-03-26 12:43:10 +0100

    updated imports

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["images.pexels.com"]
   }
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
